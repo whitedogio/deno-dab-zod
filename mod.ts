5759dc51833c9dd101175f96b3ed6f3cc17c096e
Matt Jamieson <matt@whitedog.io>
2023-05-23 05:59:37 +0000

    chore: fmt

--- mod.ts
+++ mod.ts
@@ -6,30 +6,17 @@ import { BroadcastRequest } from "./broadcast.ts";
 import { Channel } from "./channel.ts";
 import { ConnectedAgentsUpdate } from "./connected_agents_update.ts";
 import { Context } from "./context.ts";
+import { FindInstancesRequest, FindInstancesResponse } from "./find_instances.ts";
 import { FindIntentRequest, FindIntentResponse } from "./find_intent.ts";
-import {
-  FindIntentsByContextRequest,
-  FindIntentsByContextResponse,
-} from "./find_intents_by_context.ts";
+import { FindIntentsByContextRequest, FindIntentsByContextResponse } from "./find_intents_by_context.ts";
 import { Handshake, Hello } from "./hello_handshake.ts";
-import {
-  DesktopAgentMetadata,
-  ImplementationMetadata,
-} from "./implementation_metadata.ts";
+import { DesktopAgentMetadata, ImplementationMetadata } from "./implementation_metadata.ts";
 import { IntentMetadata } from "./intent_metadata.ts";
 import { IntentResolution } from "./intent_resolution.ts";
 import { Metadata } from "./metadata.ts";
 import { OpenRequest, OpenResponse } from "./open.ts";
-import {
-  RaiseIntentRequest,
-  RaiseIntentResponse,
-  RaiseIntentResultResponse,
-} from "./raise_intent.ts";
+import { RaiseIntentRequest, RaiseIntentResponse, RaiseIntentResultResponse } from "./raise_intent.ts";
 import { Source } from "./source.ts";
-import {
-  FindInstancesRequest,
-  FindInstancesResponse,
-} from "./find_instances.ts";
 
 export const parseJSON = (s: string) => messages.safeParse(JSON.parse(s));
 export const parse = (s: Messages) => messages.safeParse(s);
@@ -84,12 +71,8 @@ export type FindInstancesRequest = z.infer<typeof FindInstancesRequest>;
 export type FindInstancesResponse = z.infer<typeof FindInstancesResponse>;
 export type FindIntentRequest = z.infer<typeof FindIntentRequest>;
 export type FindIntentResponse = z.infer<typeof FindIntentResponse>;
-export type FindIntentsByContextRequest = z.infer<
-  typeof FindIntentsByContextRequest
->;
-export type FindIntentsByContextResponse = z.infer<
-  typeof FindIntentsByContextResponse
->;
+export type FindIntentsByContextRequest = z.infer<typeof FindIntentsByContextRequest>;
+export type FindIntentsByContextResponse = z.infer<typeof FindIntentsByContextResponse>;
 export type Handshake = z.infer<typeof Handshake>;
 export type Hello = z.infer<typeof Hello>;
 export type ImplementationMetadata = z.infer<typeof ImplementationMetadata>;
@@ -100,19 +83,14 @@ export type OpenRequest = z.infer<typeof OpenRequest>;
 export type OpenResponse = z.infer<typeof OpenResponse>;
 export type RaiseIntentRequest = z.infer<typeof RaiseIntentRequest>;
 export type RaiseIntentResponse = z.infer<typeof RaiseIntentResponse>;
-export type RaiseIntentResultResponse = z.infer<
-  typeof RaiseIntentResultResponse
->;
+export type RaiseIntentResultResponse = z.infer<typeof RaiseIntentResultResponse>;
 export type Source = z.infer<typeof Source>;
 
 export type Messages = z.infer<typeof messages>;
 export type RequestMessages = z.infer<typeof requestMessages>;
 export type ResponseMessages = z.infer<typeof responseMessages>;
 
-export const requestResponseMap: Record<
-  RequestMessages["type"],
-  ResponseMessages["type"] | undefined
-> = {
+export const requestResponseMap: Record<RequestMessages["type"], ResponseMessages["type"] | undefined> = {
   broadcastRequest: undefined,
   findInstancesRequest: "findInstancesResponse",
   findIntentRequest: "findIntentResponse",
