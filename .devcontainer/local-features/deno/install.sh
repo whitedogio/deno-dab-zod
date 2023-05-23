e2319f677799ee3d85def2d3c694abdd29ca1698
Matt Jamieson <matt@whitedog.io>
2023-05-23 05:35:43 +0000

    chore: fix copyright notice

--- .devcontainer/local-features/deno/install.sh
+++ .devcontainer/local-features/deno/install.sh
@@ -1,9 +1,3 @@
-#!/bin/ash
-#-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#-------------------------------------------------------------------------------------------------------------
-
 set -e
 
 apk update
